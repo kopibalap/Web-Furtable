13e67fe8bfe2b70fc5079207e0a96cdf9f4b7cb0
Arjun <77236035+kopibalap@users.noreply.github.com>
2023-04-03 03:29:02 +0700

    Add files via upload

---
+++ Web-Furtable-main ( Product  iterasi )/js/script.js
@@ -0,0 +1,38 @@
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+
+// Ketika hamburger menu diclick
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active')
+};
+
+// click di mana saja untuk menghilangkan menu
+const hamburger = document.querySelector('#hamburger-menu');
+document.addEventListener('click', function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
+
+
+const selectBtn = document.querySelector(".select-btn"),
+      items = document.querySelectorAll(".item");
+
+selectBtn.addEventListener("click", () => {
+    selectBtn.classList.toggle("open");
+});
+
+items.forEach(item => {
+    item.addEventListener("click", () => {
+        item.classList.toggle("checked");
+
+        let checked = document.querySelectorAll(".checked"),
+            btnText = document.querySelector(".btn-text");
+
+            if(checked && checked.length > 0){
+                btnText.innerText = `${checked.length} Selected`;
+            }else{
+                btnText.innerText = "Select Language";
+            }
+    });
+})
